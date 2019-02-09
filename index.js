bfbd4777c583b5e910fa18b6fa592fd722f55607
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-09 03:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+
+function add(a, b) {
+  return a + b
+}
 1 + 80
+
+function subtract(a, b) {
+  return a - b
+}
+
+
 60 - 40
-2 * 3.40
+
+function multiply(a, b) {
+  return a * b
+}
+
+2 * 3.4
+
+function divide(a, b) {
+  return a / b
+}
+
 5.0 / 2.5
+
+
+function inc(n){
+  return n + 1
+
+}
+  n++;
+  
+function dec(n){
+  return n - 1
+}
+  n--;
+
+
+
+function makeInt(n){
+  function parseInt(n){
+    return makeInt "n" = n
+}
+}
+  n = "2";
+  
